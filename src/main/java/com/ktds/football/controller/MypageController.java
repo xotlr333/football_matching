723b4e8e9fa363e3f8ad63f7dec42f91714aaf0b
xotlr333 <xotlr333@naver.com>
2024-02-11 12:44:44 +0900

    마이페이지 화면 수정 및 전체적인 화면 리펙토링

---
+++ src/main/java/com/ktds/football/controller/MypageController.java
@@ -0,0 +1,18 @@
+package com.ktds.football.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("mypage")
+public class MypageController {
+
+	@GetMapping
+	public String mypage(Model model) {
+
+
+		return "mypage/mypage";
+	}
+}
