1fd72ddb42073d260f9c8850d9517653fd97460e
taesic <xotlr333@naver.com>
2024-02-13 15:26:31 +0900

    사이드메뉴 페이지당 active 설정 추가

--- src/main/java/com/ktds/football/FootballApplication.java
+++ src/main/java/com/ktds/football/FootballApplication.java
@@ -8,8 +8,8 @@ public class FootballApplication {
 
 	// TODO : 비밀번호 암호화
 	// TODO : 문의하기
-	// TODO : 게시물 상태 마감인거 처리
-	// TODO : 날짜 지난 게시물 표시 및 상태 변경하는 기능 추가
+	// TODO : 게시물 상태 마감인거 처리 (O)
+	// TODO : 날짜 지난 게시물 표시 및 상태 변경하는 기능 추가 (O)
 	// TODO : 검색 및 카테고리 별로 게시물 나타내기
 	// TODO : 목록 없을 시 다른 화면 출력 (O)
 	public static void main(String[] args) {
